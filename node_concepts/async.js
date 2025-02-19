6d96e1c56891c63c914e3e5b0a71f4c37e247640
humanediedofanxiety <saysayeem.pr@gmail.com>
2025-02-19 23:54:00 +0600

    skibidi

--- node_concepts/async.js
+++ node_concepts/async.js
@@ -5,3 +5,31 @@ const randomNumGen = () => {
 };
 
 console.log(randomNumGen());
+
+// callbacks and event listeners
+function getResult() {
+  let result = randomNumGen();
+  return console.log(result);
+}
+
+setTimeout(getResult, 3000);
+// console.log("this will run before that");
+
+// A promise is an object returned by an async function, keywords : new Promise, async/await.
+
+const fetchPromise = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+);
+
+console.log(fetchPromise);
+
+fetchPromise.then((response) => {
+  console.log(`Received response: ${response.status}`);
+  // console.log(response);
+  const jsonFormat = response.json();
+  jsonFormat.then((date) => {
+    console.log(date[9].name);
+  });
+});
+
+console.log("Started request…");
