574cd57d43075b6627c3c8d3a3722e7e4f7872aa
humanediedofanxiety <saysayeem.pr@gmail.com>
2025-02-19 23:19:05 +0600

    learning async js

--- node_concepts/async.js
+++ node_concepts/async.js
@@ -1,1 +1,7 @@
 // async js [async programming, event-loop, non blocking I/O]
+const randomNumGen = () => {
+  let number = Math.round(Math.random() * 100 + 1);
+  return number;
+};
+
+console.log(randomNumGen());
